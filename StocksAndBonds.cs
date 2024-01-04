c5320c946f5fbe44553f3a5330ad41c03b976455
John Dunbar <u08jd21@abdn.ac.uk>
2024-01-04 23:53:51 +0000

    Documentation for StocksAndBonds v1 finalised.

--- StocksAndBonds.cs
+++ StocksAndBonds.cs
@@ -29,12 +29,12 @@ namespace FinanceFunctions
         }
 
         /// <summary>
-        /// 
+        /// Calculates the expected return of a security.
         /// </summary>
-        /// <param name="currentPrice"></param>
-        /// <param name="expectedFuturePrice"></param>
-        /// <param name="expectedDividendValue"></param>
-        /// <returns></returns>
+        /// <param name="currentPrice">Current price of the security</param>
+        /// <param name="expectedFuturePrice">Future price of the security</param>
+        /// <param name="expectedDividendValue">Expected dividend value associated with future price of security</param>
+        /// <returns>Expected return of a security</returns>
         public double ExpectedReturn(double currentPrice, double expectedFuturePrice, double expectedDividendValue)
         {
             double calculation = (expectedDividendValue + (expectedFuturePrice - currentPrice)) / currentPrice;
@@ -43,11 +43,11 @@ namespace FinanceFunctions
         }
 
         /// <summary>
-        /// 
+        /// Calculates the dividend yield of a security.
         /// </summary>
-        /// <param name="expectedDividendValue"></param>
-        /// <param name="currentPrice"></param>
-        /// <returns></returns>
+        /// <param name="expectedDividendValue">Expected dividend value associated with future price of security</param>
+        /// <param name="currentPrice">Current price of the security</param>
+        /// <returns>Dividend yield of a security</returns>
         public double DividendYield(double expectedDividendValue, double currentPrice)
         {
             double calculation = expectedDividendValue / currentPrice;
@@ -56,11 +56,11 @@ namespace FinanceFunctions
         }
 
         /// <summary>
-        /// 
+        /// Calculates the capital yield of a security.
         /// </summary>
-        /// <param name="expectedFuturePrice"></param>
-        /// <param name="currentPrice"></param>
-        /// <returns></returns>
+        /// <param name="expectedFuturePrice">Future price of the security</param>
+        /// <param name="currentPrice">Current price of the security</param>
+        /// <returns>Capital yield of a security</returns>
         public double CapitalYield(double expectedFuturePrice, double currentPrice)
         {
             double calculation = (expectedFuturePrice - currentPrice) / currentPrice;
@@ -69,12 +69,12 @@ namespace FinanceFunctions
         }
 
         /// <summary>
-        /// 
+        /// Calculates the present value of a stock.
         /// </summary>
-        /// <param name="expectedDividendValue"></param>
-        /// <param name="expectedFuturePrice"></param>
-        /// <param name="costOfEquityCapital"></param>
-        /// <returns></returns>
+        /// <param name="expectedDividendValue">Expected dividend value associated with future price of stock</param>
+        /// <param name="expectedFuturePrice">Future price of the stock</param>
+        /// <param name="costOfEquityCapital">Cost of equity capital, a.k.a opportunity cost (e.g. 44.44)</param>
+        /// <returns>Present value of a stock</returns>
         public double StockPV(double expectedDividendValue, double expectedFuturePrice, double costOfEquityCapital)
         {
             double costOfEquityCapitalNew = PercentageToDecimal(costOfEquityCapital);
@@ -83,12 +83,12 @@ namespace FinanceFunctions
         }
 
         /// <summary>
-        /// 
+        /// Calculates the future value of a stock.
         /// </summary>
-        /// <param name="expectedDividendValueYearN"></param>
-        /// <param name="expectedFuturePriceYearN"></param>
-        /// <param name="costOfEquityCapital"></param>
-        /// <returns></returns>
+        /// <param name="expectedDividendValueYearN">Expected dividend value associated with future price of stock at period N</param>
+        /// <param name="expectedFuturePriceYearN">Future price of the stock at period N</param>
+        /// <param name="costOfEquityCapital">Cost of equity capital, a.k.a opportunity cost (e.g. 44.44)</param>
+        /// <returns>Future value of a stock</returns>
         public double StockFV(double expectedDividendValueYearN, double expectedFuturePriceYearN,  double costOfEquityCapital)
         {
             double calculation = StockPV(expectedDividendValueYearN, expectedFuturePriceYearN, costOfEquityCapital);
@@ -96,12 +96,12 @@ namespace FinanceFunctions
         }
 
         /// <summary>
-        /// 
+        /// Calculates the present value of a stock by using the dividend discount model.
         /// </summary>
-        /// <param name="dividends"></param>
-        /// <param name="expectedFuturePrice"></param>
-        /// <param name="costOfEquityCapital"></param>
-        /// <returns></returns>
+        /// <param name="dividends">List of dividends received</param>
+        /// <param name="expectedFuturePrice">Future price of the stock</param>
+        /// <param name="costOfEquityCapital">Cost of equity capital, a.k.a opportunity cost (e.g. 44.44)</param>
+        /// <returns>Present value of a stock using the dividend discount model</returns>
         public double DividendDiscountModel(List<double> dividends, double expectedFuturePrice, double costOfEquityCapital)
         {
             double costOfEquityCapitalNew = PercentageToDecimal(costOfEquityCapital);
@@ -117,11 +117,11 @@ namespace FinanceFunctions
         }
 
         /// <summary>
-        /// 
+        /// Calculates the present value of a stock using the zero growth dividend discount model.
         /// </summary>
-        /// <param name="expectedDividendValue"></param>
-        /// <param name="costOfEquityCapital"></param>
-        /// <returns></returns>
+        /// <param name="expectedDividendValue">Expected dividend value</param>
+        /// <param name="costOfEquityCapital">Cost of equity capital, a.k.a opportunity cost (e.g. 44.44)</param>
+        /// <returns>Present value of a stock using the zero growth dividend discount model</returns>
         public double DividendDiscountModelZeroGrowth(double expectedDividendValue, double costOfEquityCapital)
         {
             double costOfEquityCapitalNew = PercentageToDecimal(costOfEquityCapital);
@@ -130,11 +130,11 @@ namespace FinanceFunctions
         }
 
         /// <summary>
-        /// 
+        /// Calculates the present value of a stock using the constant growth dividend discount model. This can use the current dividend value that has just been paid out, or it can use the expected future dividend payout.
         /// </summary>
-        /// <param name="dividendValue"></param>
-        /// <param name="costOfEquityCapital"></param>
-        /// <param name="growthRate"></param>
+        /// <param name="dividendValue">Dividend value</param>
+        /// <param name="costOfEquityCapital">Cost of equity capital, a.k.a opportunity cost (e.g. 44.44)</param>
+        /// <param name="growthRate">Growth rate of the stock (e.g. 44.44)</param>
         /// <param name="isFutureDividendValue"></param>
         /// <returns></returns>
         public double DividendDiscountModelConstantGrowth(double dividendValue, double costOfEquityCapital, double growthRate, bool isFutureDividendValue)
@@ -154,13 +154,13 @@ namespace FinanceFunctions
         }
 
         /// <summary>
-        /// 
+        /// Calculates the present value of a bond.
         /// </summary>
-        /// <param name="periodicCouponPayment"></param>
-        /// <param name="faceValue"></param>
-        /// <param name="costOfEquityCapital"></param>
-        /// <param name="bondLifespan"></param>
-        /// <returns></returns>
+        /// <param name="periodicCouponPayment">Periodic coupon payment issued to holder</param>
+        /// <param name="faceValue">Price paid (par value) for the bond</param>
+        /// <param name="costOfEquityCapital">Cost of equity capital, a.k.a opportunity cost (e.g. 44.44)</param>
+        /// <param name="bondLifespan">Lifespan of the bond</param>
+        /// <returns>Present value of a bond</returns>
         public double BondPV(double periodicCouponPayment, double faceValue, double costOfEquityCapital, int bondLifespan)
         {
             double costOfEquityCapitalNew = PercentageToDecimal(costOfEquityCapital);
@@ -169,12 +169,12 @@ namespace FinanceFunctions
         }
 
         /// <summary>
-        /// 
+        /// Calculates the holding period return of a bond.
         /// </summary>
-        /// <param name="currentBondPV"></param>
-        /// <param name="nextPeriodBondPV"></param>
-        /// <param name="periodicCouponPayment"></param>
-        /// <returns></returns>
+        /// <param name="currentBondPV">Present value of the bond at the current period</param>
+        /// <param name="nextPeriodBondPV">Present value of the bond at the next period</param>
+        /// <param name="periodicCouponPayment">Periodic coupon payment issued to holder</param>
+        /// <returns>Holding period return of a bond</returns>
         public double BondHoldingPeriodReturn(double currentBondPV, double nextPeriodBondPV, double periodicCouponPayment)
         {
             double calculation = ((nextPeriodBondPV - currentBondPV) + periodicCouponPayment) / currentBondPV;
@@ -182,11 +182,11 @@ namespace FinanceFunctions
         }
 
         /// <summary>
-        /// 
+        /// Calculates the return on a zero coupon bond.
         /// </summary>
-        /// <param name="expectedFutureValue"></param>
-        /// <param name="faceValue"></param>
-        /// <param name="bondLifespan"></param>
+        /// <param name="expectedFutureValue">Future value of the bond</param>
+        /// <param name="faceValue">Price paid (par value) for the bond</param>
+        /// <param name="bondLifespan">Lifespan of the bond</param>
         /// <returns></returns>
         public double ZeroCouponBondReturn(double expectedFutureValue, double faceValue, int bondLifespan)
         {
@@ -196,23 +196,10 @@ namespace FinanceFunctions
         }
 
         /// <summary>
-        /// 
+        /// Calculates the average holding period return of a security.
         /// </summary>
-        /// <param name="securityPVt">current</param>
-        /// <param name="securityPVtp">previous</param>
-        /// <param name="securityDividendt"></param>
-        /// <returns></returns>
-        public double SecurityReturn(double securityPVt, double securityPVtp, double securityDividendt)
-        {
-            double calculation = ((securityPVt - securityPVtp) + securityDividendt) / securityPVtp;
-            return calculation;
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="returnRates"></param>
-        /// <returns></returns>
+        /// <param name="returnRates">List of returns in percentage form (e.g. 44.44)</param>
+        /// <returns>Average holding period return of a security</returns>
         public double AverageHoldingPeriodReturn(List<double> returnRates)
         {
             double totalReturns = 0;
@@ -227,11 +214,11 @@ namespace FinanceFunctions
         }
 
         /// <summary>
-        /// 
+        /// Calculates the risk premium associated with a security.
         /// </summary>
-        /// <param name="expectedReturn"></param>
-        /// <param name="riskFreeRate"></param>
-        /// <returns></returns>
+        /// <param name="expectedReturn">Expected return on the security (e.g. 44.44)</param>
+        /// <param name="riskFreeRate">Risk free rate associated with the security (e.g. 44.44)</param>
+        /// <returns>Risk premium of a security</returns>
         public double RiskPremium(double expectedReturn, double riskFreeRate)
         {
             double calculation = expectedReturn - riskFreeRate;
@@ -239,10 +226,10 @@ namespace FinanceFunctions
         }
 
         /// <summary>
-        /// 
+        /// Calculates the variance of returns associated with a security.
         /// </summary>
-        /// <param name="returnRates"></param>
-        /// <returns></returns>
+        /// <param name="returnRates">List of returns in percentage form (e.g. 44.44)</param>
+        /// <returns>Variance of returns of a security</returns>
         public double VarianceOfReturn(List<double> returnRates)
         {
             double averageReturn = AverageHoldingPeriodReturn(returnRates);
@@ -259,10 +246,10 @@ namespace FinanceFunctions
         }
 
         /// <summary>
-        /// 
+        /// Calculates the standard deviation of returns associated with a security.
         /// </summary>
-        /// <param name="returnRates"></param>
-        /// <returns></returns>
+        /// <param name="returnRates">List of returns in percentage for (e.g. 44.44)</param>
+        /// <returns>Standard deviation of returns of a security</returns>
         public double StandardDeviationOfReturn(List<double> returnRates)
         {
             double calculation = Math.Sqrt(VarianceOfReturn(returnRates));
@@ -270,12 +257,11 @@ namespace FinanceFunctions
         }
 
         /// <summary>
-        /// 
+        /// Calculates the return of a series of assets in a portfolio.
         /// </summary>
-        /// <param name="weightPerAsset"></param>
-        /// <param name="averageReturnRates"></param>
-        /// <returns></returns>
-        /// <exception cref="Exception"></exception>
+        /// <param name="weightPerAsset">List of proportions of assets held in percentage form (e.g. 44.44)</param>
+        /// <param name="averageReturnRates">List of average return rates of assets held in percentage form (e.g. 44.44)</param>
+        /// <returns>Portfolio return</returns>
         public double PortfolioReturn(List<double> weightPerAsset, List<double> averageReturnRates)
         {
             int sizeWeight = weightPerAsset.Count;
@@ -295,11 +281,11 @@ namespace FinanceFunctions
         }
 
         /// <summary>
-        /// 
+        /// Calculates the systematic risk of an asset against the market it exists in.
         /// </summary>
-        /// <param name="covarianceBetweenAssetAndMarket"></param>
-        /// <param name="marketVariance"></param>
-        /// <returns></returns>
+        /// <param name="covarianceBetweenAssetAndMarket">Covariance between the security and the market it operates in</param>
+        /// <param name="marketVariance">Market variance</param>
+        /// <returns>Systematic risk of an asset</returns>
         public double SystematicRisk(double covarianceBetweenAssetAndMarket, double marketVariance)
         {
             double calculation = covarianceBetweenAssetAndMarket / marketVariance;
@@ -307,13 +293,13 @@ namespace FinanceFunctions
         }
 
         /// <summary>
-        /// 
+        /// Calculates abnormal returns gained on an asset against the market it exists in.
         /// </summary>
-        /// <param name="returnOnInvestment"></param>
-        /// <param name="riskFreeRate"></param>
-        /// <param name="marketReturn"></param>
-        /// <param name="systematicRisk">Beta coefficient</param>
-        /// <returns></returns>
+        /// <param name="returnOnInvestment">Asset's return on investement (e.g. 44.44)</param>
+        /// <param name="riskFreeRate">Risk free rate associated with the market (e.g. 44.44)</param>
+        /// <param name="marketReturn">Market return (e.g. 44.44)</param>
+        /// <param name="systematicRisk">Beta coefficient, a.k.a systematic risk</param>
+        /// <returns>Abnormal returns gained on an asset</returns>
         public double AbnormalReturn(double returnOnInvestment, double riskFreeRate, double marketReturn, double systematicRisk)
         {
             double roiNew = PercentageToDecimal(returnOnInvestment);
